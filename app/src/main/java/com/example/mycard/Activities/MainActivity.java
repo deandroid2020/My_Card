5ff85c7759ffdd7e1c7a807e53a143ef566db3fd
RahimDesk <mth_41@hotmail.com>
2021-02-04 17:09:51 +0300

    add // test

--- app/src/main/java/com/example/mycard/Activities/MainActivity.java
+++ app/src/main/java/com/example/mycard/Activities/MainActivity.java
@@ -15,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        
+        // test
 
 
       //  startActivity(new Intent(getApplicationContext() , Student.class));
