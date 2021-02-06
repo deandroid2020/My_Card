a69e720ecf4a28eb8f2f633fde2753f821b38f60
RahimDesk <mth_41@hotmail.com>
2021-02-06 17:50:39 +0300

    Added Menu

--- app/src/main/java/com/example/mycard/Activities/MainActivity.java
+++ app/src/main/java/com/example/mycard/Activities/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.widget.Toolbar;
 import com.example.mycard.R;
-import com.example.mycard.helper.CircularTextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +17,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
-        // test
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+
+
+
+        Button button = findViewById(R.id.btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext() , Student.class));
+            }
+        });
+
 
 
       //  startActivity(new Intent(getApplicationContext() , Student.class));
 
     }
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
 }
