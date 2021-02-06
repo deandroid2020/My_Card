cf853c998d34f608e4257fa561e9ae6a75e602f6
RahimDesk <mth_41@hotmail.com>
2021-02-06 19:07:27 +0300

    added footer

--- app/src/main/java/com/example/mycard/Activities/Forget_Card.java
+++ app/src/main/java/com/example/mycard/Activities/Forget_Card.java
@@ -12,5 +12,8 @@ public class Forget_Card extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget__card);
+
+
+
     }
 }
